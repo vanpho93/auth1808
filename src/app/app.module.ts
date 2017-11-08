51e464084c4fbbe47696e980730fe5653edf3a77
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 20:00:34 +0700

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PublicComponent } from './public/public.component';
 import { PrivateComponent } from './private/private.component';
+import { AuthGuard } from './auth.guard';
+import { AboutComponent } from './about/about.component';
 
 const routerConfig: Routes = [
   { path: 'public', component: PublicComponent },
-  { path: 'private', component: PrivateComponent }
+  { path: 'private', component: PrivateComponent, canActivate: [AuthGuard] },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     PublicComponent,
-    PrivateComponent
+    PrivateComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routerConfig)
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
