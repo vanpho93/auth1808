4256363278c053d68fe981c6d83f8c707321fff7
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 20:14:58 +0700

    client

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -10,6 +10,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       const token = localStorage.getItem('token');
+      console.log(token);
       if (token) {
         return true;
       }
