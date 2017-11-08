bd110e0ffc8d885abdeea243e62380bf0a152d08
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 19:43:03 +0700

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
+
+const routerConfig: Routes = [
+  { path: 'public', component: PublicComponent },
+  { path: 'private', component: PrivateComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    PrivateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
